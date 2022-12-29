d4b14a9c82526112c3b46d29e847d53c3b774a31
makarovandrej <makarov.andrey.nn@icloud.com>
2022-12-29 23:39:26 +0100

    Implementation simple calculator with parsing get parameters

--- src/main/java/com/makarov/springcourse/controllers/FirstController.java
+++ src/main/java/com/makarov/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.makarov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        System.out.println(request.getParameter("name"));
+    public String helloPage(HttpServletRequest request, Model model) {
+        model.addAttribute("message", "Hello, " + request.getParameter("name") + " " + request.getParameter("surname"));
         return "first/hello";
     }
 
@@ -22,5 +23,35 @@ public class FirstController {
         System.out.println(name);
         System.out.println(surname);
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculate(HttpServletRequest request, Model model) {
+        int a = Integer.parseInt(request.getParameter("a"));
+        int b = Integer.parseInt(request.getParameter("b"));
+        int result;
+
+        String action = request.getParameter("action");
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("result", result);
+
+        return "/first/calculator";
     }
 }
