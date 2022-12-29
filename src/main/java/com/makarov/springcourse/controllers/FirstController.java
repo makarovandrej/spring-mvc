edecbe01940dcb38dce170bd7e9a475f2a878781
makarovandrej <makarov.andrey.nn@icloud.com>
2022-12-29 23:11:36 +0100

    RequestParam and HttpServletRequest - how to get request params from GET

---
+++ src/main/java/com/makarov/springcourse/controllers/FirstController.java
@@ -0,0 +1,26 @@
+package com.makarov.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(HttpServletRequest request) {
+        System.out.println(request.getParameter("name"));
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println(name);
+        System.out.println(surname);
+        return "first/goodbye";
+    }
+}
