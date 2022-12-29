6646129ac5d622e99bf05b20f610fea539ae396f
makarovandrej <makarov.andrey.nn@icloud.com>
2022-12-29 18:35:31 +0100

    Use java configuration instead web.xml and applicationContext.xml

---
+++ src/main/java/com/makarov/springcourse/config/SpringDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.makarov.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+/**
+ * Alternative web.xml
+ */
+public class SpringDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
