4773567cc3c8286a6517236e96697f6e9a2aa842
makarovandrej <makarov.andrey.nn@icloud.com>
2022-12-30 03:07:43 +0100

    DAO person

---
+++ src/main/java/com/makarov/springcourse/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package com.makarov.springcourse.dao;
+
+import com.makarov.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "James"));
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+        people.add(new Person(++PEOPLE_COUNT, "Andrew"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+}
